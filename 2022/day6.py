07079f7739f48936d2f8d080ea53067e4bb68416
schnurly <daniel.esche@bwi.de>
2022-12-06 09:08:16 +0100

    new file:   2022/06_input.txt 	new file:   2022/day6.py 	new file:   2022/day6_2.py

---
+++ 2022/day6.py
@@ -0,0 +1,27 @@
+with open('C:\\temp\\Advent_of_Code\\2022\\06_input.txt',"r") as file:
+    input=file.read()
+
+validationPattern = []
+
+index=0
+for char in input:
+    validationPattern.append(char)
+    if(len(validationPattern)>4):
+        validationPattern.pop(0)
+    if(len(validationPattern)==4):
+        if(index > 6):
+            charCounter = {}
+            for c in validationPattern:
+                if c not in charCounter:
+                    charCounter[c] =0
+                charCounter[c]+=1
+            isValid=True
+            for c in charCounter:
+                if(charCounter[c]>1):
+                    isValid=False
+            if(isValid):
+                print("found on index " +str(index))
+                print(index+1)
+                print(validationPattern)
+                break
+    index+=1
